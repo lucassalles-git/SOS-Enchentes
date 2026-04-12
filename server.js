86ce5d72885947ac41b7011dcbf627d8e41bf0da
lucassalles-git <lucasmssp@gmail.com>
2026-04-12 00:16:14 -0300

    Atualiza rotas para listar registros de desaparecidos e adicionar novos registros

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
         `);
 });
 
-//Rota para listar os abrigos
+//Rota para listar os registrados
 app.get("/registros", async (req, res) => {
   const db = await bancoDados(); //chamando a função do database
 
@@ -27,11 +27,30 @@ app.get("/registros", async (req, res) => {
   res.json(todosOsRegistros);
 });
 
+//Rota específica por nome
+app.get("/registros/:nome", async (req, res) => {
+  const { nome } = req.params;
+  const db = await bancoDados();
+  const pessoaEspecifica = await db.all(
+    `SELECT * FROM registros WHERE nome = ?`,
+    [nome],
+  );
 
+  res.json(pessoaEspecifica);
+});
 
+//POST novo registro de desaparecido
+app.post("/registros", async (req, res) => {
+  const { nome, idade, abrigo, endereco } = req.body;
+  const db = await bancoDados();
 
+  await db.run(
+    `INSERT INTO registros(nome, idade, abrigo, endereco) VALUES (?, ?, ?, ?)`,
+    [nome, idade, abrigo, endereco],
+  );
 
-
+  res.send(`Cadastro de desaparecido: ${nome}, de ${idade} de idade`)
+});
 
 const PORT = 3000;
 
